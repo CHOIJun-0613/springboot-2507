92d5ff767a2a456c0dca432506ddd6af71459c0f
최준 <cjonyou@naver.com>
2025-07-16 17:26:20 +0900

    서비스 포트 변경

---
+++ demo/src/main/java/com/example/demo/controller/PostController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+    @GetMapping("/")
+    public String postList(){
+        return "블로그 목록입니다.";
+    }
+
+}
