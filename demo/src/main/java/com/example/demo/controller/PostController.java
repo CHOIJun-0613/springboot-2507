fb5f5dbf5255a0642a0f7fd077e0d3fca95068ac
최준 <cjonyou@naver.com>
2025-07-16 18:06:43 +0900

    수정

--- demo/src/main/java/com/example/demo/controller/PostController.java
+++ demo/src/main/java/com/example/demo/controller/PostController.java
@@ -1,15 +1,25 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/posts")
 public class PostController {
-    @GetMapping("/")
+    @GetMapping("/list")
     public String postList(){
         return "블로그 목록입니다.";
+    }
+    @GetMapping("/{postId}")
+    public String postDetail(@PathVariable String postId){
+        return postId + "번 블로그 상세입니다.";
+    }
+    @GetMapping("/update")
+    public String updatePost(@RequestParam String postId){
+        return postId + "번 블로그 수정 페이지입니다.....";
     }
 
 }
